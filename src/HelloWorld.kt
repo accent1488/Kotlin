b76f7640acbd452230e4cd2754744696f3ab4a6f
accent1488 <rkfldkd@gmail.com>
2021-07-23 00:43:10 +0400

    String templates

--- src/HelloWorld.kt
+++ src/HelloWorld.kt
@@ -1,3 +1,9 @@
 fun main(args: Array<String>) {
-    print("Hello, World!")
+    var name: String = "Vlados"
+    var a:Int = 1
+    var b:Int = 2
+
+    println("Hello, $name")
+    println("a + b = ${a+b}")
+
 }
